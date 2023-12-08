d4c05a9fe3e19aeba849ae13c32d7ee6bcd095b6
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2023-12-08 15:10:36 -0500

    .

--- Frontend/src/components/General/General.jsx
+++ Frontend/src/components/General/General.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import '../css/General.css';
 
 const General = () => {
   return (
-    <div>
-        <h1>Portfolio de Candidatos destacados</h1>
-        <p className='descrip'>
+    <div className='descrip'>
+        <h3>Portfolio de Candidatos destacados</h3>
+        <p>
             Queremos acercarte algunos de nuestros mejores candidatos entrevistados. Por su experiencia, 
             formación, actitud y disponibilidad entre otros factores, representan una excelente oportunidad para tu equipo. 
             Te invitamos a conocerlos y que nos cuentes si pueden aportar valor a tu compañía.
